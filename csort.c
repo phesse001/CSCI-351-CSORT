2da31481274e36a116a0f022a1fa3ea6b28d47a2
PHESSE001 <phesse001@vplincsci087.ad.csbsju.edu>
2020-11-07 19:11:07 -0600

    parallelized with working 2nd loop

--- csort.c
+++ csort.c
@@ -1,13 +1,14 @@
 /* assert */
 #include <assert.h>
 
+/* OpenMP API */
+#include <omp.h>
+
 /* EXIT_SUCCESS, rand */
 #include <stdlib.h>
 
 /* strtol */
 #include <stdio.h>
-
-#include <omp.h>
 
 static int
 csort(unsigned const k,
@@ -15,37 +16,43 @@ csort(unsigned const k,
       unsigned const * const in,
       unsigned       * const out)
 {
-  unsigned * const count = calloc(k + 1, sizeof(*count));
+  int num_threads;
+
+# pragma omp parallel
+  num_threads = omp_get_num_threads();
+
+  unsigned * const count = calloc(num_threads * k + 1, sizeof(*count));
   if (NULL == count) {
     return -1;
   }
-  //increments the count of each number (i.e. 0,1,1,4,3 -> [1,2,0,1,1])
-  double const ts = omp_get_wtime();
-  #pragma omp parallel for
-  for (unsigned i = 0; i < n; i++) {
-    #pragma omp atomic
-    count[in[i]]++;
-  }
 
-  //adds up counts (i.e. [1,2,0,1,1] -> [0,1,3,3,4])
+  double ts1 = omp_get_wtime();
+# pragma omp parallel for
+  for (unsigned i = 0; i < n; i++) {
+    printf("%d\n",in[i] * num_threads + omp_get_thread_num());
+    count[in[i] * num_threads + omp_get_thread_num()]++;
+  }
+  double te1 = omp_get_wtime();
+  printf("timer 1: %lf\n", te1 - ts1);
+
+  double ts2 = omp_get_wtime();
   unsigned total = 0;
-  for (unsigned i = 0; i <= k; i++) {
+  for (unsigned i = 0; i <= k * num_threads; i++) {
     unsigned const counti = count[i];
     count[i] = total;
     total += counti;
   }
-  //uses each input element as index for count, accessing where the input element should be
-  //put in final array (i.e. 0,1,1,4,3 -> count[0] = 0 -> out[0] = 0 ... in[3] = 4, count[4] = 4, out[4] = 4)
-  #pragma omp parallel for
+  double te2 = omp_get_wtime();
+  printf("timer 2: %lf\n", te2 - ts2);
+
+  double ts3 = omp_get_wtime();
+# pragma omp parallel for
   for (unsigned i = 0; i < n; i++) {
-    #pragma omp critical
-    {
-    out[count[in[i]]] = in[i];
-    count[in[i]]++;
-    }
+    out[count[in[i] * num_threads + omp_get_thread_num()]] = in[i];
+    count[in[i] * num_threads + omp_get_thread_num()]++;
   }
-  double const te = omp_get_wtime();
-  printf("elapsed time %lf\n",te-ts);
+  double te3 = omp_get_wtime();
+  printf("timer 3: %lf\n", te3 - ts3);
 
   free(count);
 
@@ -69,12 +76,9 @@ main(int argc, char *argv[]) {
     a[i] = rand() % (1u << k);
   }
 
-
   /* Sort array */
   int const ret = csort(1u << k, n, a, b);
-
   assert(0 == ret);
-
 
   /* Validate sorted array */
   for (unsigned i = 1; i < n; i++) {
