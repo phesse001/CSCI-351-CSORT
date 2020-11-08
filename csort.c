cf80c92bb8d8975e043d7e0629700a33051f8b00
PHESSE001 <phesse001@vplincsci049.ad.csbsju.edu>
2020-11-08 15:16:42 -0600

    looking into loop behavior

--- csort.c
+++ csort.c
@@ -16,46 +16,71 @@ csort(unsigned const k,
       unsigned const * const in,
       unsigned       * const out)
 {
+  double ts = omp_get_wtime();
   omp_set_dynamic(0);     // Explicitly disable dynamic teams
-  omp_set_num_threads(1); // Use 4 threads for all consecutive parallel regions
+  omp_set_num_threads(2); // Use 4 threads for all consecutive parallel regions
   int num_threads;
   # pragma omp parallel
   num_threads = omp_get_num_threads();
+  printf("number of threads = %d\n", num_threads);
 
-  unsigned * const count = calloc(num_threads * k + 1, sizeof(*count));
+  unsigned * const count = calloc(num_threads * k+1, sizeof(*count));
   if (NULL == count) {
     return -1;
   }
-
-  double ts1 = omp_get_wtime();
-# pragma omp parallel for
+  printf("original count array:\n[");
   for (unsigned i = 0; i < n; i++) {
-    printf("%d\n",in[i] * num_threads + omp_get_thread_num());
+    printf("%u,",in[i]);
+  }
+  printf("]\n");
+
+  //double ts1 = omp_get_wtime();
+  # pragma omp parallel for
+  for (unsigned i = 0; i < n; i++) {
     count[in[i] * num_threads + omp_get_thread_num()]++;
   }
-  double te1 = omp_get_wtime();
-  printf("timer 1: %lf\n", te1 - ts1);
+  //double te1 = omp_get_wtime();
+  //printf("timer 1: %lf\n", te1 - ts1);
+  printf("count after first loop:\n[");
+  for (unsigned i = 0; i < num_threads*k +20; i++) {
+    printf("%u,",count[i]);
+  }
+  printf("]\n");
 
-  double ts2 = omp_get_wtime();
+  //double ts2 = omp_get_wtime();
   unsigned total = 0;
   for (unsigned i = 0; i <= k * num_threads; i++) {
     unsigned const counti = count[i];
     count[i] = total;
     total += counti;
   }
-  double te2 = omp_get_wtime();
-  printf("timer 2: %lf\n", te2 - ts2);
+  //double te2 = omp_get_wtime();
+  //printf("timer 2: %lf\n", te2 - ts2);
 
-  double ts3 = omp_get_wtime();
-# pragma omp parallel for
+  printf("count after second loop:\n[");
+  for (unsigned i = 0; i < num_threads*k +20; i++) {
+    printf("%u,",count[i]);
+  }
+  printf("]\n");
+
+  //double ts3 = omp_get_wtime();
+  # pragma omp parallel for
   for (unsigned i = 0; i < n; i++) {
     out[count[in[i] * num_threads + omp_get_thread_num()]] = in[i];
     count[in[i] * num_threads + omp_get_thread_num()]++;
   }
-  double te3 = omp_get_wtime();
-  printf("timer 3: %lf\n", te3 - ts3);
+  //double te3 = omp_get_wtime();
+  //printf("timer 3: %lf\n", te3 - ts3);
+
+  printf("count after final loop:\n[");
+  for (unsigned i = 0; i <n; i++) {
+    printf("%u,",out[i]);
+  }
+  printf("]\n");
 
   free(count);
+  double te = omp_get_wtime();
+  printf("timer: %lf\n",te-ts);
 
   return 0;
 }
